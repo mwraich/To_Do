a00c7adbefd0c19122c6d124f709cacda750672c
mandeep <mwraich28@gmail.com>
2016-05-01 16:13:38 -0400

    Created seeds for todo list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Todo.create!(title: 'grocery shopping', notes: 'pickles, eggs, red onion')
+Todo.create!(title: 'wash the car')
+Todo.create!(title: 'register kids for school', notes: 'Register Kira for Ruby Junior High and Caleb for Rails High School')
+Todo.create!(title: 'check engine light', notes: 'The check engine light is on in the Tacoma')
+Todo.create!(title: 'dog groomers', notes: 'Take Pinky and Redford to the groomers on Wednesday the 23rd')
